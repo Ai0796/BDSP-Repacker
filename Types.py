cf14d066d728fc1a91dc5d8036752a990ea6c49c
Alden Mo <mo.alden25@gmail.com>
2022-08-24 17:43:25 -0400

    Removed type.json in favor of a .txt file

---
+++ Types.py
@@ -0,0 +1,44 @@
+
+class Types():
+    
+    TYPEFILE = "types.txt"
+    DEFAULTTYPES = [
+        "MonoBehaviour",
+        "AssetBundleManifest",
+        "GameObject",
+        "BoxCollider",
+        "Transform",
+        "ParticleSystem",
+        "AssetBundle",
+        "AnimationClip"
+    ]
+    
+    def __init__(self) -> None:
+        self.typeNames = self.DEFAULTTYPES
+    
+    def readTypes(self) -> None:
+        self.typeNames = []
+        if os.path.eixsts(self.TYPEFILE):
+            f = open(self.TYPEFILE, "r")
+            lines = f.readlines()
+
+            for line in lines:
+                line = line.strip()
+                if line.startswith("#") or len(line) == 0:
+                    continue
+                else:
+                    print("Read Type:", line)
+                    self.typeNames.append(line)
+
+            if len(self.typeNames) == 0:
+                self.typeNames = self.DEFAULTTYPES
+        else:
+            print("Type.txt not found, using default values")
+                
+    def getTypeNames(self):
+        return self.typeNames
+                
+                
+if __name__ == "__main__":
+    type = Types()
+    print(type.getTypeNames())
